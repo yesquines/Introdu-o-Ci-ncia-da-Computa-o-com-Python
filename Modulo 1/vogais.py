66d4e979cad7de464ba80bdeadcf9fdbd4f5ffbb
Yago Ésquines <36844032+yesquines@users.noreply.github.com>
2022-07-17 20:12:06 +0100

    change: simplified code

--- Modulo 1/vogais.py
+++ Modulo 1/vogais.py
@@ -1,5 +1,10 @@
 def vogal(x):
-	if x == "a" or x == "e" or x == "i" or x == "o" or x == "u" or x == "A" or x == "E" or x == "I" or x == "O" or x == "U":
-		return True
-	else:
-		return False
+    vogais = ['a','e','i','o','u']
+    if x.lower() in vogais:
+        return True
+    else:
+        return False
+
+v = input('Digite uma vogal: ')
+
+print(vogal(v))
